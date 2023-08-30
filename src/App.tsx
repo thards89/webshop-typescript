1d20b7e3b7b657c648d78f027f34a7480f95848f
thards89 <thards89@gmail.com>
2023-08-30 18:52:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>Amazonas TS</header>
+        <main></main>
+        <footer>All rights reserved</footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
